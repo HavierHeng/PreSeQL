5cd0af00082b97062240d393c3f86956e7ea7897
Javier Heng <javier_2001@live.com.sg>
2025-04-18 21:32:23 +0800

    updated radix tree to match page no of 16 bits)

--- src/algorithm/radix_tree.h
+++ src/algorithm/radix_tree.h
@@ -1,4 +1,4 @@
-// Radix tree is for finding and freeing free pages in O(nlogn) time
+// Radix tree is for finding and freeing free pages in O(log n) time
 // This is used in a few parts of the code: 
 // 1) Finding Free pages in Pager - For Reuse of pages, to prevent fragmentation
 // 2) Free Pages in Journal if Transaction is used - For Reuse, such as when pages are dirty, we make a copy
@@ -6,15 +6,15 @@
 
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include <unistd.h>
 
-#define RADIX_BITS 8
+#define RADIX_BITS 4
 #define RADIX_SIZE (1 << RADIX_BITS)
-
 
 typedef struct RadixNode {
     struct RadixNode *children[RADIX_SIZE];  // next level
-    int32_t status; // valid at leaves only (-1 = not free)
+    int16_t status; // valid at leaves only (-1 = not free)
 } RadixNode;
 
 typedef struct RadixTree {
@@ -25,14 +25,14 @@ RadixTree* radix_tree_create();
 void radix_tree_destroy(RadixTree* tree);
 
 // Basic Radix Tree operations
-void radix_tree_insert(RadixTree *tree, uint32_t page_no, int32_t slot);
-void radix_tree_delete(RadixTree *tree, uint32_t page_no);
-int32_t radix_tree_pop_min(RadixTree *tree);
-int32_t radix_tree_lookup(RadixTree *tree, uint32_t page_no);
+void radix_tree_insert(RadixTree *tree, uint16_t page_no, int16_t slot);
+void radix_tree_delete(RadixTree *tree, uint16_t page_no);
+int16_t radix_tree_pop_min(RadixTree *tree);
+int16_t radix_tree_lookup(RadixTree *tree, uint16_t page_no);
 
 // Walk while executing callback function - e.g free
-void radix_tree_walk(RadixNode *node, uint32_t prefix, int depth, void (*cb)(uint32_t page, int slot));
+void radix_tree_walk(RadixTree *tree, void (*cb)(uint16_t page, int16_t slot));
 
 // Conversion between freelist and radix tree
-void freelist_to_radix(RadixTree* tree, uint32_t* freelist, size_t count);
-size_t radix_to_freelist(RadixTree* tree, uint32_t* freelist, size_t max_size);
+void freelist_to_radix(RadixTree* tree, uint16_t* freelist, size_t count);
+size_t radix_to_freelist(RadixTree* tree, uint16_t* freelist, size_t max_size);
