f09d80f3712e1b483645b28662d3522dc016753d
Javier Heng <javier_2001@live.com.sg>
2025-04-20 06:38:55 +0800

    AHHHHHHHHHH

--- src/pager/db/index/page.h
+++ src/pager/db/index/page.h
@@ -1,6 +1,10 @@
 #ifndef PRESEQL_PAGER_DB_INDEX_PAGE_H
 #define PRESEQL_PAGER_DB_INDEX_PAGE_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <strings.h>
+#include <stdint.h>
 // Index page operations
 
 #endif /* PRESEQL_PAGER_DB_INDEX_PAGE_H */
