f7bd5ce3714a9c38e28029ce505b2f9b939c8f07
Javier Heng <javier_2001@live.com.sg>
2025-04-20 19:46:57 +0800

    Il est orange

--- src/pager/constants.h
+++ src/pager/constants.h
@@ -21,8 +21,8 @@
 
 
 /* Max Size of Page Headers in Bytes (regardless of type - strictly pad to this size in structs) */
-#define MAX_PAGE_HEADER_SIZE 127  /* Max page header size in bytes - this makes it easier to know when the page data starts and ends. This affects the size of the union of headers */
-#define MAX_JOURNAL_HEADER_SIZE 31  /* Max Size of journal header in bytes 
+#define MAX_PAGE_HEADER_SIZE 16  /* Max page header size in bytes - this makes it easier to know when the page data starts and ends. This affects the size of the union of headers */
+#define MAX_JOURNAL_HEADER_SIZE 16  /* Max Size of journal header in bytes 
                         - Fixed size to make it easier to ensure enough space is reserved to align to 4096B page */
 #define MAX_USABLE_PAGE_SIZE (PAGE_SIZE - MAX_PAGE_HEADER_SIZE - MAX_JOURNAL_HEADER_SIZE) /* Limit usable bytes for data in data pages, 
 excluding the overhead from header of DB page and journal page. 
@@ -37,15 +37,13 @@ Aim for page alignment by padding. */
 
 
 /* Generic Page flags */
-// Page status flags - Apply by AND with the flags field
-#define PAGE_DIRTY 0x01
-#define PAGE_FREE  0x02
 
 // Database flags
 #define DB_READONLY 0x01
 #define DB_JOURNAL_ENABLED 0x02
 #define DB_CORRUPT 0x04
 
+#define FREE_SLOT_LIST_SIZE 16  /* Logically I won't really need to exceed this value that much - if it gets reused */
 
 
 /* B+ Tree Index Page */
@@ -54,18 +52,30 @@ Aim for page alignment by padding. */
                         In reality, this is an upper bound since the key sizes always determine the effective order of the tree 
                         - e.g if a truncated TEXT of 16 bytes is always stored with an overflow pointer of 8 bytes (64-bit system)
                         Max keys is (ORDER-1) for internal nodes, ORDER for leaf nodes */
+#define MAX_DATA_PER_INDEX_SLOT 16  /* Max data for a slot used in Index Page slotted page. A slot here is fixed sized.
+                        This value is lower than data page as we want a high effective order for good branching and faster search. 
+                        This determines the max size of the search key for the B+ Tree.
+                        All TEXT are truncated to this value if does exceed.
+                        NULL will be effectively \0 for all 16 bytes.
+                        INT (64-bit signed) will only use the first 8 bytes and leave the remaining 8 bytes blank. INTs are encoded to a positive range for lexicographic comparison in Index pages (though the true value in data page is still signed as twos complement).
+                        - if size is exceeded in index page, it will go into an Overflow Page, and the pointer to overflow page will be set to the overflow page number and chunk.
+                        */
 
 /* Data Page */
-#define MAX_DATA_PER_SLOT 16  /* Max data for a slot used in both Data Page and Index Page slotted page. 
+#define MAX_DATA_PER_DATA_SLOT 256  /* Max data for a slot used in both Data Page and Index Page slotted page. A slot in data page is variable in size.
+                        This value is way higher than index slot, as we want to store actual row data (255 Bytes gives us up to a VARCHAR(255))
                         All TEXT are truncated to this value. 
                         NULL will be effectively \0 for all 16 bytes.
                         INT (64-bit signed) will only use the first 8 bytes and leave the remaining 8 bytes blank.
-                        - if exceeded in data page or index page, it will go into an Overflow Page, and the pointer to overflow page will be set to the overflow page number and chunk.
+                        - if this limit is exceeded in data page, it will go into an Overflow Page, and the pointer to overflow page will be set to the overflow page number and chunk.
                         */
-#define SLOT_VACCUM_BATCH_SIZE 15 /* Threshold for number of deletes to execute a VACCUM operation on slotted pages 
-of Index Page, Data Page and Overflow Page.
-This is as VACCUM operation to move slots are expensive, but fragmentation of slots due to delete operations can make the pages more sparse.
+
+
+#define VACCUM_BATCH_SIZE 15  /* Every how many deletes to check if all pages need to be VACCUMed. VACCUM Operations are expensive but fragmentation slows things down and can make pages sparse/half-filled. */
+
+#define INDEX_SLOT_VACCUM_SIZE 255 /* Threshold for free continguous space before performing a VACCUM and rehashing to all Radix tree buckets of Index Page
 */
+
 
 /* Overflow Page */
 #define MAX_OVERFLOW_CHUNKS 255  /* Max chunks that can be stored in an Overflow page, a chunk is defined as effectively a slot of data */
--- src/pager/db/db_format.h
+++ src/pager/db/db_format.h
@@ -9,46 +9,101 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
-#include "config.h"
+#include "pager/constants.h"
 
+/* Shared Flags values */
+#define PAGE_INDEX_INTERNAL  = 0x01  // 0000 0001 - B+ Root or Internal Node Page. Internal nodes point to other Internal nodes or Leaf nodes.
+#define PAGE_INDEX_LEAF      = 0x02  // 0000 0010 - B+ Leaf Node Page - We distinguish this to separate concerns since Leaf nodes point to Data Pages.
+#define PAGE_DATA            = 0x04  // 0000 0100 - Data Page
+#define PAGE_OVERFLOW        = 0x08  // 0000 1000 - Overflow Page
+#define PAGE_DIRTY           = 0x10  // 0001 0000 - Page has been modified since the last sync or commit. In practice, this isn't needed since `msync` is done after all modifications.
+#define PAGE_FREE            = 0x20  // 0010 0000 - Page is marked as free and can be reused. In practice, we don't use this since we have the Radix tree loaded in memory.
+#define PAGE_COMPACTIBLE     = 0x40  // 0100 0000 - This flag indicates whether the slots in the page is eligible for compaction. Set when changes are made to the page, but unset after VACCUM. Can hint to page begin as compacted as it can be and should be skipped over during VACCUM.
+#define PAGE_PINNED          = 0x80  // 1000 0000 - Page is pinned in memory can cannot be evicted - In practice, this isn't used since `mmap` deals with paging on its own via the kernel.
 
-// Forward declarations
-typedef struct BTreePageHeader BTreePageHeader;
-typedef struct DataPageHeader DataPageHeader;
-typedef struct OverflowPageHeader OverflowPageHeader;
+/* Guide to use flags later for forgetful people:
+// To set the flag (e.g., mark as dirty)
+flags |= PAGE_DIRTY;
 
-#include "pager/db/index/page_format.h"
-#include "pager/db/data/page_format.h"
-#include "pager/db/overflow/page_format.h"
+// To check if a page is an index page
+if (flags & PAGE_INDEX) {
+    // It's an index page
+}
 
-typedef enum {
-    DATA,
-    INDEX,
-    OVERFLOW
-} PageType;
-
-// Header different for each of 3 Page types 
-typedef union {
-    BTreePageHeader btree;
-    DataPageHeader data;
-    OverflowPageHeader overflow;
-} PageTypeHeader;
+// To clear the "pinned" flag (unpin a page)
+flags &= ~PAGE_PINNED;
+*/ 
 
 // Generic Page Header
+// Fun fact: Because all the pages are slotted page based - they actually share the same headers
+// The only difference between page types is what is stored in each slot which I will go into later - but really does not matter
+//
 typedef struct {
+    // Page related
     uint16_t page_id;  // Max of 2^16 = 65535 pages
-    uint8_t dirty;
-    uint8_t free;
-    PageType type;
-    PageTypeHeader type_specific;  // These have to be padded to same size in each implementation 
+    uint16_t ref_counter;  // To know when free can be done
+    uint8_t flag;  // See above for PAGE flags
+
+    // Slot Related
+    uint16_t free_start;  // Start of free space - does not exceed 16 bits - 4096 is 12 bits only
+    uint16_t free_end;  // End of free space
+    uint16_t free_total;  // Available free space to grow slots into
+    uint8_t highest_slot;  // Fallback if no entries in free slot list
+    uint8_t free_slot_list[FREE_SLOT_LIST_SIZE];  // Track and reuse free slots as much as possible
+
+    // Pad to MAX_PAGE_HEADER_SIZE = 16 in bytes
+    uint8_t reserved[3];
 } PageHeader;
 
-// Page Memory, aligned to PAGE_SIZE (4KB usually) 
+
+
+/* Stored in slot entry table after Header */
+typedef struct {
+    uint8_t slot_id;  // Up to 256 slots per page
+    uint64_t offset;  // Offset to start of slot data
+    uint64_t size;  // Size of slot data - same for all slots in the same page
+} SlotEntry;
+
+
+
+
+typedef struct {
+    uint16_t next_page_id;  // Page of overflow page
+    uint16_t next_chunk_id;  // Page of chunk in overflow page that contains more data
+} OverflowPointer;
+
+/* The only difference between page types is the Slot data they store 
+ * No need define structs for these - just do.
+ * */
+
+/* For both Internal and Leaf Index Pages */
+typedef struct {
+    uint8_t key[MAX_DATA_PER_INDEX_SLOT];   // Up to MAX_DATA_PER_INDEX_SLOT (16) bytes of truncated key - key are encoded specially to be lexicographically sortable
+    uint16_t next_page_id;  // Pointer to next Index page (Internal/leaf) that might contain data
+    uint8_t next_slot_id;  // Pointer to slot in next Index Page - Up to 256 slots per page
+    OverflowPointer overflow;  // Overflow pointer - null if no overflow
+} IndexSlotData;
+
+/* For Data Page */
+typedef struct {
+    uint8_t *data;  // Up to MAX_DATA_PER_DATA_SLOT (255) bytes of truncated data - the name is different to reflect that values are stored as is
+    OverflowPointer overflow;  // Overflow pointer - if row cannot fit within the data given
+} DataSlotData;
+
+
+/* For Overflow Page chunks - Still a slotted page */
+typdef struct {
+    uint8_t data
+} OverflowSlotData;
+
+// Page Memory, aligned to PAGE_SIZE (4096Bytes usually) 
 // The usable space is further capped to prevent journal from exceeding PAGE_SIZE
 typedef struct {
-    PageHeader header;
-    uint8_t payload[MAX_DATA_BYTES];  // The data depends on the page type 
+    PageHeader header;  // 16 bytes
+    uint8_t data[MAX_USABLE_PAGE_SIZE];  // 4064 bytes The data depends on the page type - its filled with SlotEntry and Index/Data/OverflowSlotData types
+    uint8_t reserved[MAX_JOURNAL_HEADER_SIZE];  // 16 bytes - reserved for Journal later
 } DBPage;
+
 
 #endif
 
