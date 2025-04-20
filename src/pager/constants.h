08a4588c30646a9720f17b66c25b310f9661a2e6
Javier Heng <javier_2001@live.com.sg>
2025-04-20 16:20:20 +0800

    OIAA OIAA MEOW MEOW

--- src/pager/constants.h
+++ src/pager/constants.h
@@ -5,23 +5,34 @@
 
 
 /* Page Sizes - Check OS setting */
-#define PAGE_SIZE 4096
+#define PAGE_SIZE 4096  /* Usually either 4KB or 8KB, but more commonly 4KB */
 #define MAX_PAGES 65535  /* 2^16 so all page counts are represented by uint_16 */
+#define ARCH_BITS 64  /* Safe to assume this is the case for all new hardware and OSes - this also means this might not build on some old RPis and Microcontrollers lol git guud */
+#define POINTER_SIZE (BIT_ARCH/8)  /* On 64-bit hardware and OS (yes use the right version), this value will always be 8 bytes */
 
-/* Max Size of Page Headers in Bytes (regardless of type - pad if necessary) */
-#define JOURNAL_HEADER_SIZE 32  /* Size of journal header in bytes - Fixed size to make it easier to ensure enough space is reserved to align to 4096B page */
-#define MAX_DATA_BYTES (PAGE_SIZE - JOURNAL_HEADER_SIZE) /* Limit usable bytes for data in data pages, 
+
+/* File names */
+#define DB_FILE_EXTENSION ".pseql"  /* Main DB file extension */
+#define JOURNAL_FILE_EXTENSION ".pseql-journal"  /* Journal file extension */
+#define DATABASE_NAME_LENGTH 6  /* .pseql including the dot */
+#define JOURNAL_NAME_LENGTH  14 /* .pseql-journal including the dot */
+#define OS_MAX_FILE_NAME 255
+#define MAX_FILE_NAME (OS_MAX_FILE_NAME - JOURNAL_NAME_LENGTH) /* Max Database /Journal Name (minus the largest possible extension size which is .pseql-journal)*/
+
+
+/* Max Size of Page Headers in Bytes (regardless of type - strictly pad to this size in structs) */
+#define MAX_PAGE_HEADER_SIZE 127  /* Max page header size in bytes - this makes it easier to know when the page data starts and ends */
+#define MAX_JOURNAL_HEADER_SIZE 31  /* Max Size of journal header in bytes 
+                        - Fixed size to make it easier to ensure enough space is reserved to align to 4096B page */
+#define MAX_USABLE_PAGE_BYTES (PAGE_SIZE - MAX_JOURNAL_HEADER_SIZE) /* Limit usable bytes for data in data pages, 
                                                         since journal has some overhead itself, 
                                                         but we aim for page alignment */
 
-#define MAX_HEADER_SIZE 255  /* Max page header size in bytes - this makes it easier to know when the page data starts and ends */
 
 /* Base Metadata Page */
 #define MAGIC_NUMBER_SIZE 8  /* Magic number is a fixed sized field */
 #define MAGIC_NUMBER "SQLSHITE"  /* For magic number */
-#define DB_FILE_EXTENSION ".pseql"  /* Main DB file extension */
-#define JOURNAL_FILE_EXTENSION ".pseql-journal"  /* Journal file extension */
-#define FREE_PAGE_LIST_BATCH_SIZE 10  // the number of changes before saving is set by a BATCH_SIZE variable
+#define FREE_PAGE_LIST_BATCH_SIZE 10  /* the number of changes to Radix tree before flushing back to disk free page list */
 
 
 /* Generic Page flags */
@@ -37,7 +48,10 @@
 
 
 /* B+ Tree Index Page */
-#define BTREE_ORDER 256 /* Max keys per B+ Tree node - i.e max number of children a node can have/fanout. 
+#define BTREE_ORDER 256 /* Max keys per B+ Tree node 
+                        - i.e max number of children a node can have/fanout. 
+                        In reality, this is an upper bound since the key sizes always determine the effective order of the tree 
+                        - e.g if a truncated TEXT of 16 bytes is always stored with an overflow pointer of 8 bytes (64-bit system)
                         Max keys is (ORDER-1) for internal nodes, ORDER for leaf nodes */
 
 /* Data Page */
@@ -47,5 +61,6 @@
 #define MAX_OVERFLOW_CHUNKS 256  /* Max chunks that can be stored in an Overflow page */
 
 /* Catalog Pages */
+#define MAX_TABLE_NAME_LENGTH 255  /* Including null terminator */
 
 #endif 
